7fe2a7d9a88cedf71210bb702d0ff15c512a5270
Viet Anh To <vietto@amazon.com>
2016-05-11 08:50:24 -0700

    Add common definitions

---
+++ common/CommonDefinitions.h
@@ -0,0 +1,32 @@
+#include <stdint.h>
+
+typedef char                                    CHAR;
+typedef short                                   WCHAR;
+typedef uint8_t                                 UINT8;
+typedef int8_t                                  INT8;
+typedef uint16_t                                UINT16;
+typedef int16_t                                 INT16;
+typedef uint32_t                                UINT32;
+typedef int32_t                                 INT32;
+typedef uint64_t                                UINT64;
+typedef int64_t                                 INT64;
+typedef double                                  DOUBLE;
+typedef float                                   FLOAT;
+typedef UINT8                                   BYTE;
+typedef INT32                                   BOOL;
+
+
+typedef CHAR*                                   PCHAR;
+typedef WCHAR*                                  PWCHAR;
+typedef UINT8*                                  PUINT8;
+typedef INT8*                                   PINT8;
+typedef UINT16*                                 PUINT16;
+typedef INT16*                                  PINT16;
+typedef UINT32*                                 PUINT32;
+typedef INT32*                                  PINT32;
+typedef UINT64*                                 PUINT64;
+typedef INT64*                                  PINT64;
+typedef DOUBLE*                                 PDOUBLE;
+typedef FLOAT*                                  PFLOAT;
+typedef BYTE*                                   PBYTE;
+typedef BOOL*                                   PBOOL;
