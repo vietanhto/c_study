d938209c5e9d6a7f6547b8ba0f57b91029e815a9
Viet Anh To <vietanhto.se@gmail.com>
2016-05-18 23:59:38 -0700

    removed list sorting as it is slow.

--- utils/src/Algorithms.c
+++
@@ -1,9 +0,0 @@
-#include "Algorithms.h"
-
-STATUS List_bubble_sort(PDoubleLinkedList list, ListCompare listCompare) {
-
-}
-
-PDoubleLinkedList List_merge_sort(PDoubleLinkedList list, ListCompare listCompare) {
-
-}
